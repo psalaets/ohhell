e7e6ce030728eefe0bd4b8dcfdf9b5851a7bfdfe
Paul Salaets <psalaets@gmail.com>
2012-09-21 17:34:41 -0400

    game creates rounds based on high round

--- js/Game.js
+++ js/Game.js
@@ -1,6 +1,24 @@
-function Game() {
+function Game(highRound) {
+    this.rounds = [];
+    this.createRounds(highRound);
+
     this.players = [];
 }
+
+Game.prototype.createRounds = function(highRound) {
+    //Ascending rounds
+    for(var i = 1; i < highRound; i++) {
+        this.rounds.push(new Round("Round " + i));
+    }
+
+    //High round
+    this.rounds.push(new Round("Round " + highRound));
+
+    //Descending rounds
+    for(var i = highRound - 1; i > 0; i--) {
+        this.rounds.push(new Round("Round " + i));
+    }
+};
 
 Game.prototype.addPlayer = function(player) {
     this.players.push(player);
@@ -8,4 +26,8 @@ Game.prototype.addPlayer = function(player) {
 
 Game.prototype.getPlayers = function() {
     return this.players;
+};
+
+Game.prototype.getRounds = function() {
+    return this.rounds;
 };
--- test/GameSpec.js
+++ test/GameSpec.js
@@ -1,14 +1,15 @@
 describe("Game", function() {
-    var game;
-
-    beforeEach(function() {
-        game = new Game();
-    });
-
     it("should be able to add players", function() {
+        var game = new Game();
         game.addPlayer('bob');
         game.addPlayer('tom');
 
         expect(game.getPlayers()).toEqual(['bob', 'tom']);
     });
+
+    it("should have enough rounds to play to highRound and back down to 1", function() {
+        var game = new Game(8);
+
+        expect(game.getRounds().length).toEqual(15);
+    })
 });
