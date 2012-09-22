963120b5089dc20e493922e9888a4aa820ac47b9
Paul Salaets <psalaets@gmail.com>
2012-09-22 10:16:42 -0400

    printing out the entire game

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,13 @@
 function GameController($scope) {
-    $scope.name = 'bob';
+    $scope.game = new Game(['bob', 'joe', 'tom']);
+    $scope.game.start(3);
+
+    //Helper for populating bid select
+    $scope.zeroTo = function(max) {
+        var values = [];
+        for(var i = 0; i <= max; i++) {
+            values.push(i);
+        }
+        return values;
+    };
 }
