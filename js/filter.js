7da2eb725bc0981b2085a1a49a4afedf7042f431
Paul Salaets <psalaets@gmail.com>
2012-09-22 22:33:58 -0400

    using angular built-in for number check

--- js/filter.js
+++ js/filter.js
@@ -2,7 +2,7 @@ angular.module("ohhell.filter", []).
     //Puts a +/- on numbers that aren't 0
     filter("sign", function() {
         return function(value) {
-            if(value === 0 || value) {
+            if(angular.isNumber(value)) {
                 if(value > 0) {
                     return "+" + value;
                 } else if(value < 0) {
