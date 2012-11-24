cc7be12d00e7839afe397ae6016859ea2fefb724
Paul Salaets <psalaets@gmail.com>
2012-11-24 12:27:07 -0500

    show total rounds when setting game up

--- js/controller.js
+++ js/controller.js
@@ -27,6 +27,10 @@ controller('SetupController', ['$scope', 'navService', 'Game', 'storageService',
         }
     });
 
+    $scope.totalRounds = function() {
+        return ($scope.maxRound * 2) - 1;
+    };
+
     $scope.startGame = function() {
         var game = Game.create($scope.players, $scope.maxRound);
         storageService.save(game);
