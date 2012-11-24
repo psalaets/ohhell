754e01a8b6ef3bd6b11b450e6763208ae549269f
Paul Salaets <psalaets@gmail.com>
2012-11-24 15:02:13 -0500

    reworked game setup, one comma-delimited field for player names was confusing

--- js/controller.js
+++ js/controller.js
@@ -17,6 +17,18 @@ controller('SetupController', ['$scope', 'navService', 'Game', 'storageService',
     $scope.maxRoundAllowed = 8;
     $scope.players = [];
 
+    $scope.addPlayer = function(name) {
+        if(name) {
+            $scope.players.push(name);
+            $scope.playerName = '';
+        }
+    };
+
+    //Remove by array index
+    $scope.removePlayerAtPosition = function(index) {
+        $scope.players.splice(index, 1);
+    };
+
     $scope.$watch("players.length", function(newValue, oldValue, scope) {
         if(newValue < 2) {
             scope.message = "Need at least two players to begin";
