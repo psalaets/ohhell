6f126b0cdce9c96dc0a40bfe2fac0f714bc04361
Paul Salaets <psalaets@gmail.com>
2015-03-21 21:36:37 -0400

    break out a few more filters

--- app/filter.js
+++ app/filter.js
@@ -1,16 +1,4 @@
 angular.module("app.filter", []).
-    //Puts a +/- on numbers that aren't 0
-    filter("sign", function() {
-        return function(value) {
-            if(angular.isNumber(value)) {
-                if(value > 0) {
-                    return "+" + value;
-                } else if(value < 0) {
-                    return value.toString();
-                }
-            }
-        }
-    }).
     filter("upto", function() {
         return function(start, end) {
             var arr = [];
@@ -18,33 +6,6 @@ angular.module("app.filter", []).
                 arr.push(i);
             }
             return arr;
-        };
-    }).
-    filter("place", function() {
-        var placeSuffixes = {
-            "1": "st",
-            "2": "nd",
-            "3": "rd",
-            "11": "th",
-            "12": "th",
-            "13": "th"
-        };
-
-        return function(value) {
-            return value + (placeSuffixes[value] || "th");
-        }
-    }).
-    filter('sentence', function() {
-        return function(values) {
-            if(values.length === 1) {
-                return values[0];
-            } else if(values.length === 2) {
-                return values[0] + ' and ' + values[1];
-            } else if(values.length > 2) {
-                var valuesCopy = values.slice(0);
-                var last = valuesCopy.pop();
-                return valuesCopy.join(', ') + ' and ' + last.toString();
-            }
         };
     }).
     filter('timeAgo', function() {
