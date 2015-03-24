bb5f1ca199e4b9158acbd3eea89dc9dd9ce7efee
Paul Salaets <psalaets@gmail.com>
2015-03-24 00:13:29 -0400

    bring back vague-time for 'time ago' filter

--- app/saved-games/time-ago.filter.js
+++ app/saved-games/time-ago.filter.js
@@ -1,9 +1,12 @@
-;(function iife(angular) {
+;(function iife(angular, vagueTime) {
   angular.module('app.saved-games').filter('timeAgo', timeAgo);
 
   function timeAgo() {
     return function(value) {
-      return 'a while ago';
+      return vagueTime.get({
+        to: value,
+        units: 'ms'
+      });
     };
   }
-})(angular);
+})(angular, vagueTime);
